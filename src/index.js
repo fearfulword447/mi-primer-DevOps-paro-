cc3ef0250ab7394b1bfc01849ceadcd61bb00b12
fearfulword447 <tomas.guerra@alumnos.ucn.cl>
2026-06-01 17:16:01 -0400

    correccion de vulnerabilidad

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-// Código intencionalmente vulnerable para la Actividad 2
-// Flujo completo: Origen no confiable -> Destino peligroso
+// Codigo corregido
+
 
 function mostrarMensajeDelUsuario() {
-    // 1. ORIGEN (Source): Tomamos datos directamente de la URL (parámetro 'nombre')
     const parametrosUrl = new URLSearchParams(window.location.search);
     const inputDelUsuario = parametrosUrl.get('nombre');
     
     const contenedor = document.getElementById('mensaje');
     
-    // 2. DESTINO (Sink): Inyectamos el dato sin limpiarlo
-    // Esto es un DOM XSS de libro. ¡CodeQL no podrá ignorarlo!
-    contenedor.innerHTML = "Hola, " + inputDelUsuario;
+    // SOLUCION: Usar textContent en lugar de innerHTML.
+    contenedor.textContent = "Hola, " + inputDelUsuario;
 }
 
-// Ejecutamos la función
 mostrarMensajeDelUsuario();
